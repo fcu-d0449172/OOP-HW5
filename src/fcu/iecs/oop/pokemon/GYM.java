2504e1f3eb08c8a8258ef0cf1dccaf40d043edee
ASUS <ASUS@192.168.0.4>
2016-11-27 19:58:56 +0800

    oop-hw5

--- src/fcu/iecs/oop/pokemon/GYM.java
+++ src/fcu/iecs/oop/pokemon/GYM.java
@@ -1,10 +1,20 @@
 package fcu.iecs.oop.pokemon;
 
+
 public class GYM {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void fight(Pokemon p1, Pokemon p2)
+	{
+		int winner = (int) (Math.random()*2); 
+		
+		if(winner==0)
+		{
+			p1.setCp(p1.getCp()+500);
+			System.out.println("Winner is "+p1.getName()+" ,CP:"+p1.getCp());
+		}
+		else
+		{
+			p2.setCp(p2.getCp()+500);
+			System.out.println("Winner is "+p2.getName()+" ,CP:"+p2.getCp());
+		}
 	}
-
 }
--- src/fcu/iecs/oop/pokemon/Pokemon.java
+++ src/fcu/iecs/oop/pokemon/Pokemon.java
@@ -1,10 +1,21 @@
 package fcu.iecs.oop.pokemon;
 
 public class Pokemon {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private final String name;
+	private int cp;
+	
+	public Pokemon(String name, int cp) {
+		this.name = name;
+		this.cp = cp;
 	}
-
+	
+	public String getName() {
+		return this.name;
+	}
+	public int getCp() {
+		return this.cp;
+	}
+	public void setCp(int cp) {
+		this.cp = cp;
+	}
 }
